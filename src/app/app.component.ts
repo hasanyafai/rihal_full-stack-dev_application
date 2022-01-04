d2cf5c9389ba3ce7fbd5a0cde802571525ec5d63
Hassan AlYafai <hasan.m.alyafai@gmail.com>
2022-01-04 17:03:30 +0400

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'rihal_full-stack-dev_application';
+}
